ee0bf2df966204922296b83c966373cb3c084de5
Karina Almeida <Karina.Almeida161@outlook.com>
2024-09-23 20:41:53 -0300

    Primeiros passos de PHP

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang=" PT - BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+</head> 
+<body>   
+<form action="script.php" method="post">
+    
+<label for="nome">Nome</label>
+<input type="texer" name="nota2" id="nota2">
+<label for="nota2">Nota 2</label>
+<input type="number" name="nota3" id="nota3">
+<label for="nota3">Nota 3</label>
+<input type="number" name="nota1" id="nota1">
+<label for="nota4">Nota 4</label>
+<input type="number" name="nota4" id="nota4">
+
+<button type="submit">Enviar</button>
+
+
+</form>
+
+
+
+</body>
+</html>
